c564659f3f4458f60a1a05ea02ac58dca2038a90
Xiaoshi Xu <xxu@reputation.com>
2016-09-12 19:27:49 +0800

    commit 2

---
+++ src/main/java/xxu/nlp/NLPTest.java
@@ -0,0 +1,13 @@
+package xxu.nlp;
+
+public class NLPTest {
+    
+    public static void main(String[] args) {
+        String [] etypes = {" Token ", " Emoticon ", " Collocation "};
+//        GateAnalyzer analyzer = new GateAnalyzer ( etypes );
+//        String str = "Its a big project to lay on the line , "
+//            + " but it won ’t be long now. :-) ";
+//        displayTokensWithDetails ( analyzer , str);
+    }
+
+}
